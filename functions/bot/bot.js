e0b86e5e97a77fa5de0a4bf342197df076ef66f3
eyob2m <eyob2etemariam@gamil.com>
2023-08-07 21:14:23 +0300

    ol

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,6 +1,8 @@
 
+require('dotenv').config()
+
 const telegraf =  require('telegraf')
-module.exports = bot = new  telegraf('6468224337:AAGrt8G8xYxWtKND6yx5PvtGZgvDP1xKRCU')
+module.exports = bot = new  telegraf(process.env.API)
 
 const Start =  require('./start')
 Start()
